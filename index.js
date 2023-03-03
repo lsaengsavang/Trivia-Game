4e1402b0c714750202e2d9a7a3614ae1f7bf8323
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-03 00:34:28 -0800

    Bare bones game

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let username = window.prompt('Please enter your name:')
+let userName = window.prompt('Please enter your name:')
+let userScore = 0
 
-for(let i = 0; i < questions.length; i++) {
-  console.log (questions[i])
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
 }
+
+window.alert('Your score is: '+userScore)
